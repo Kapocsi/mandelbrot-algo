0f06ad80ee9900883b072fc3a61261ea1bb4afde
Thomas Kapocsi <thomaskapocsi@Thomass-MacBook-Pro.local>
2023-03-10 16:31:50 -0700

    complete with 2D multi thread + zoom/pann

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
-use num::{
-    complex::{self, Complex64},
-    traits::Pow,
-};
-use std::f64::consts;
+use std::fmt::format;
+use std::process::Command;
 
-use image::{imageops::brighten, Rgb};
-use image::{GrayImage, ImageBuffer, Luma, RgbImage};
+use num::traits::Inv;
+use num::{complex::Complex64, ToPrimitive};
+
+use image::imageops::{brighten, invert};
+use image::{ImageBuffer, RgbImage};
 
 use rayon::prelude::*;
 
-const GRID_X: u32 = 16384;
-const GRID_Y: u32 = GRID_X;
-const scale_factore: f64 = GRID_X as f64 / 4.0;
-
-fn progress_bar(current: i32, max: i32) {
-    let progress: f64 = (current as f64 / max as f64) * 100.0;
-    print!(
-        "\r{}{}",
-        (0..(progress as i32 / 2)).map(|_| "=").collect::<String>(),
-        (0..(50 - (progress as i32 / 2)))
-            .map(|_| " ")
-            .collect::<String>()
-    );
-    print!(" {}%", progress);
-    if progress == 100.0 {
-        println!("");
-    }
-}
-#[inline]
-fn get_escape_value(c: Complex64, limit: u8) -> u8 {
+fn get_escape_value(c: Complex64, limit: u16) -> u16 {
     let mut value = Complex64 { re: 0.0, im: 0.0 };
     {
         for i in 0..=limit {
@@ -41,38 +22,54 @@ fn get_escape_value(c: Complex64, limit: u8) -> u8 {
     }
 }
 
+fn generate_mandelbrot_grid(resolution: u32, offset_x: f64, offset_y: f64, zoom: f64) -> Vec<u16> {
+    // disbale offset
+
+    (0..=resolution.pow(2))
+        .into_par_iter()
+        .filter_map(|i| {
+            let i = i.to_u32()?;
+            let col = 2.0 * ((i % resolution).to_f64()? / resolution.to_f64()? - 0.5);
+            let row = 2.0 * ((i / resolution).to_f64()? / resolution.to_f64()? - 0.5);
+
+            let value = get_escape_value(
+                // Convert in to row and column then zoom in
+                Complex64 {
+                    re: (col / zoom) - offset_x,
+                    im: (row / zoom) - offset_y,
+                },
+                765,
+            );
+            Some(value)
+        })
+        .collect::<Vec<u16>>()
+}
+
 fn main() {
-    let mut img: RgbImage = ImageBuffer::new(GRID_X, GRID_Y);
+    let image_size: u32 = (2 as u32).pow(12);
+    let grid = generate_mandelbrot_grid(image_size, 0.91, 0.234, 10000.0);
+    let mut img: RgbImage = ImageBuffer::new(image_size + 1, image_size + 1);
 
-    for cmp_cord in 0..GRID_Y {
-        let cmp_cord_v: f64 = (cmp_cord as f64 - (GRID_Y as f64 / 2.0)) / (GRID_Y / 2) as f64;
-        //((real_cord as f64 - (GRID_X as f64 / 2.0)) / (GRID_X as f64 / 2.0)) - 0.5;
-
-        let completion_steps: Vec<u8> = (0..GRID_X)
-            .into_par_iter()
-            .enumerate()
-            .map(|(index, _)| {
-                let real_cord_val =
-                    ((index as f64 - (GRID_X as f64 / 2.0)) / (GRID_X as f64 / 2.0)) - 0.5;
-                return get_escape_value(
-                    Complex64 {
-                        re: real_cord_val,
-                        im: cmp_cord_v,
-                    },
-                    255,
-                );
-            })
-            .collect();
-
-        for (i, val) in completion_steps.iter().enumerate() {
-            img.put_pixel(i as u32, cmp_cord, image::Rgb([*val, *val, *val]))
+    // Split number up to 768 into 3 buckets that flow into each other
+    let spiller_over = |n: u16| -> [u8; 3] {
+        //assert!(n <= 765);
+        if n <= 255 {
+            [n as u8, 0, 0]
+        } else if (n >= 255) & (n <= 510) {
+            [255, (n - 256) as u8, 0]
+        } else {
+            [255, 255, (n - 510) as u8]
         }
+    };
 
-        progress_bar(cmp_cord as i32, GRID_Y as i32)
+    for (i, p) in grid.iter().enumerate() {
+        let i = i as u32;
+        img.put_pixel(i % image_size, i / image_size, image::Rgb(spiller_over(*p)))
     }
 
     img = brighten(&img, 10);
 
-    img.save(format!("result_{}x{}.png", GRID_X, GRID_Y))
-        .unwrap();
+    img.save(format!("result_{}.png", image_size)).unwrap();
+
+    Command::new("open").arg(format!("result_{}.png", image_size));
 }
