0333d05e9af226d4c7c8656f6bd6b8d220b4bdf3
Thomas Kapocsi <thomaskapocsi@Thomass-MacBook-Pro.local>
2023-03-10 19:18:23 -0700

    changed alignment and color

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fmt::format;
 use std::process::Command;
 
+use num::complex::ComplexFloat;
 use num::traits::Inv;
 use num::{complex::Complex64, ToPrimitive};
 
@@ -24,7 +25,6 @@ fn get_escape_value(c: Complex64, limit: u16) -> u16 {
 
 fn generate_mandelbrot_grid(resolution: u32, offset_x: f64, offset_y: f64, zoom: f64) -> Vec<u16> {
     // disbale offset
-
     (0..=resolution.pow(2))
         .into_par_iter()
         .filter_map(|i| {
@@ -35,36 +35,35 @@ fn generate_mandelbrot_grid(resolution: u32, offset_x: f64, offset_y: f64, zoom:
             let value = get_escape_value(
                 // Convert in to row and column then zoom in
                 Complex64 {
-                    re: (col / zoom) - offset_x,
+                    re: (col / zoom) - (offset_x + 0.5),
                     im: (row / zoom) - offset_y,
                 },
                 765,
             );
-            Some(value)
+            Some(value + (row.abs() + col.abs()).to_u16()?)
         })
         .collect::<Vec<u16>>()
 }
 
 fn main() {
     let image_size: u32 = (2 as u32).pow(12);
-    let grid = generate_mandelbrot_grid(image_size, 0.91, 0.234, 10000.0);
-    let mut img: RgbImage = ImageBuffer::new(image_size + 1, image_size + 1);
-
+    let grid = generate_mandelbrot_grid(image_size, 0.0, 0.0, 1.0);
+    let mut img = ImageBuffer::new(image_size + 1, image_size + 1);
     // Split number up to 768 into 3 buckets that flow into each other
     let spiller_over = |n: u16| -> [u8; 3] {
         //assert!(n <= 765);
         if n <= 255 {
-            [n as u8, 0, 0]
+            [0, n as u8, 0]
         } else if (n >= 255) & (n <= 510) {
-            [255, (n - 256) as u8, 0]
+            [0, (n - 256) as u8, 255]
         } else {
-            [255, 255, (n - 510) as u8]
+            [(n - 510) as u8, 255, 255]
         }
     };
 
     for (i, p) in grid.iter().enumerate() {
         let i = i as u32;
-        img.put_pixel(i % image_size, i / image_size, image::Rgb(spiller_over(*p)))
+        img.put_pixel(i % image_size, i / image_size, image::Rgb(spiller_over(*p)));
     }
 
     img = brighten(&img, 10);
